b3335c3d199cb131c1bec348202db164f9cb6179
Titus Wormer <tituswormer@gmail.com>
2021-07-23 13:23:50 +0200

    Update `xo`

--- script/build.js
+++ script/build.js
@@ -1,5 +1,5 @@
-import fs from 'fs'
-import path from 'path'
+import fs from 'node:fs'
+import path from 'node:path'
 import {svgTagNames} from 'svg-tag-names'
 
 const casing = svgTagNames.filter((d) => d !== d.toLowerCase())
